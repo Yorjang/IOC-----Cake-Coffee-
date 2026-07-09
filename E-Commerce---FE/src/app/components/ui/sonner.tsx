00acf49f3cfaa30be438e8856e7ee9de88d4fc30
Yor4 <hoangbanggiang04@gmail.com>
2026-07-09 14:51:57 +0700

    feat: disable all toast alert notifications globally

--- E-Commerce---FE/src/app/components/ui/sonner.tsx
+++ E-Commerce---FE/src/app/components/ui/sonner.tsx
@@ -5,23 +5,7 @@ import { Toaster as Sonner } from "sonner";
 import type { ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
-  return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      duration={2000}
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
-      {...props}
-    />
-  );
+  return null;
 };
 
 export { Toaster };
